63f9f5d6db66605ec886c043569372548ead8a21
Nikolay Slavov <nikolay@slavov.org>
2019-02-18 12:05:08 +0200

    Add mix info

--- mix.exs
+++ mix.exs
@@ -1,14 +1,29 @@
 defmodule PaginatorTG.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :paginator_tg,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+
+      # Docs
+      name: "PaginatorTG",
+      source_url: "https://github.com/tapgiants/paginator_tg",
+      homepage_url: "https://github.com/tapgiants/paginator_tg",
+      docs: [
+        source_ref: "v#{@version}",
+        main: "PaginatorTG",
+        canonical: "http://hexdocs.pm/paginator_tg",
+        source_url: "https://github.com/tapgiants/paginator_tg"
+      ]
     ]
   end
 
@@ -32,6 +47,20 @@ defmodule PaginatorTG.MixProject do
       {:ecto, "~> 3.0"},
       {:ecto_sql, "~> 3.0"},
       {:postgrex, ">= 0.0.0", optional: true}
+    ]
+  end
+
+  defp description do
+    """
+    Cursor based pagination wrapper arount Paginator package.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Tap Giants"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/tapgiants/paginator_tg"}
     ]
   end
 end
